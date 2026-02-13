653c57a7b9d37f227f0435868da8da0f7cdac971
Diaz Kautsar <diazkautsar77@gmail.com>
2026-02-13 09:18:18 +0700

    fold setting

--- lua/core/api.lua
+++ lua/core/api.lua
@@ -1,4 +1,10 @@
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldenable = true
-vim.o.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	command = "hi Folded guibg=NONE ctermbg=NONE",
+})
