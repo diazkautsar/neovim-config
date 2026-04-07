0de8678df7e37af685dd6886c14abcc54ffb8308
Diaz Kautsar <diazkautsar77@gmail.com>
2026-04-07 13:59:06 +0700

    add claude code

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,3 +19,5 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
